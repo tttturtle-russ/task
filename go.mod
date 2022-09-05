fa03a8dc5033e5d3e918ecfd15f81a0659024963
tttturtle-russ <u202112205@hust.edu.cn>
2022-09-05 19:05:43 +0800

    first step on task project

---
+++ go.mod
@@ -0,0 +1,19 @@
+module task
+
+go 1.19
+
+require (
+	github.com/fatih/color v1.13.0
+	github.com/manifoldco/promptui v0.9.0
+	github.com/urfave/cli/v2 v2.14.0
+)
+
+require (
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+)
---
+++ task.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/fatih/color"
+	"github.com/manifoldco/promptui"
+	"github.com/urfave/cli/v2"
+	"io"
+	"log"
+	"os"
+	"time"
+)
+
+var tasks Tasks
+var logFile *os.File
+
+type Task struct {
+	Name       string    `json:"name"`
+	Deadline   time.Time `json:"deadline"`
+	Todo       string    `json:"todo"`
+	Importance string    `json:"importance"`
+}
+type Tasks struct {
+	Title string
+	Tasks []Task
+}
+
+const layout = "20060102"
+
+func init() {
+	err := initTaskList()
+	if err != nil {
+		log.Println(err)
+	}
+}
+func main() {
+	app := cli.NewApp()
+	app.Name = "task"
+	app.Usage = "record your task list"
+
+	app.Version = "1.0.0"
+	app.Description = "a simple tool that record your todo list"
+	app.Authors = append(app.Authors, &cli.Author{
+		Name: "tttturtle-russ",
+	})
+	app.Commands = cli.Commands{
+		{
+			Name:        "show",
+			Usage:       "show your tasks",
+			Description: "`show` command shows all the tasks that have been recorded",
+			Action:      showTask,
+		},
+		{
+			Name:   "add",
+			Usage:  "add task",
+			Action: addTask,
+		},
+	}
+	app.ExitErrHandler = func(c *cli.Context, err error) {
+		logFile.Close()
+	}
+	app.Run(os.Args)
+}
+
+func initTaskList() error {
+	tasks.Tasks = make([]Task, 0)
+	tasks.Title = "Tasks"
+	var err error
+	logFile, err = os.OpenFile("F\\GoWorkspace\\src\\task\\log\\log.log", os.O_APPEND|os.O_RDWR|os.O_CREATE, 0777)
+	multiWriter := io.MultiWriter(os.Stdout, logFile)
+	log.SetOutput(multiWriter)
+	log.SetPrefix("[task]")
+	log.SetFlags(log.Ldate | log.Ltime)
+	list, err := os.OpenFile("F:\\GoWorkspace\\src\\task\\list.json", os.O_RDWR|os.O_CREATE, 0777)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	defer list.Close()
+	var data []byte
+	data, err = io.ReadAll(list)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	if len(data) == 0 {
+		return nil
+	}
+	err = json.Unmarshal(data, &tasks)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
+}
+
+func showTask(c *cli.Context) error {
+	if len(tasks.Tasks) == 0 {
+		fmt.Println("nothing to do right now")
+		return nil
+	}
+	color.Cyan("%-30s%-30s%-30s%-30s", "Name", "ToDo", "Deadline", "Importance")
+	for _, task := range tasks.Tasks {
+		var importanceString string
+		switch task.Importance {
+		case "‼️very important":
+			importanceString = color.RedString(task.Importance)
+		case "❗️just so so":
+			importanceString = color.CyanString(task.Importance)
+		case "❕ doesn't matter":
+			importanceString = color.BlackString(task.Importance)
+		}
+		color.Yellow("%-30s%-30s%-30s%-30s", task.Name, task.Todo, task.Deadline.Format("2006-01-02 15-04"), importanceString)
+	}
+	return nil
+}
+
+func addTask(c *cli.Context) error {
+	// TODO 参数列表实现
+	var task Task
+	fmt.Println("input task name")
+	_, err := fmt.Scanln(&task.Name)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	fmt.Println("input what todo")
+	_, err = fmt.Scanln(&task.Todo)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	fmt.Println("input task deadline format:YYYYMMdd")
+	var ddl string
+	_, err = fmt.Scanln(&ddl)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	task.Deadline, err = time.ParseInLocation(layout, ddl, time.Local)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	importantStr := color.RedString("‼️very important")
+	sosoStr := color.CyanString("❗️just so so")
+	dontMatterStr := color.BlackString("❕ doesn't matter")
+	p := promptui.Select{
+		Label: "how important it is?",
+		Items: []string{
+			importantStr, sosoStr, dontMatterStr,
+		},
+	}
+	_, result, err := p.Run()
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	task.Importance = result
+	list, err := os.OpenFile("F:\\GoWorkspace\\src\\task\\list.json", os.O_RDONLY|os.O_CREATE|os.O_TRUNC, 0777)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	defer list.Close()
+	tasks.Tasks = append(tasks.Tasks, task)
+	data, err := json.Marshal(tasks)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	_, err = list.Write(data)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
+}
